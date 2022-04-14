133bb2659f6ab0c87aed880f87f867b87166c6dd
Mantas Skara <mantassk@unity3d.com>
2022-04-14 13:39:28 +0300

    some updates to frenzy

---
+++ FeedingFrenzy/Assets/FishPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishPlayer : MonoBehaviour
+{
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        // apply forward input
+        Vector3 movementHorizontal = Input.GetAxis("Horizontal") * new Vector3(1, 0, 0) * speed;
+        Vector3 movementVertical = Input.GetAxis("Vertical") * new Vector3(0, 1, 0) * speed;
+
+        transform.position += movementHorizontal * Time.deltaTime;
+        transform.position += movementVertical * Time.deltaTime;
+
+        // GetComponent<Rigidbody2D>().position += velocity * Time.deltaTime;
+    }
+}
